ccdb816e07b4a5323ba91d07a81c9d2c3d9d5bad
frontcook <442747096@qq.com>
2019-02-22 11:03:19 +0800

    新建文件夹

--- src/components/Opensource.vue
+++ src/components/Opensource.vue
@@ -16,7 +16,7 @@ export default {
   name: 'Opensource',
   mounted () {
     this.init()
-    this.$http.get('static/resource/Readme.md')
+    this.$http.get('static/res/Readme.md')
       .then(res => {
         this.$refs.marked.innerHTML = marked(res.data)
       })
